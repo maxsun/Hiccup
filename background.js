9d8f6a5a2b1593771c43b02e3b973d6922cd0196
Shaurya Sanghvi <shaurya_s@berkeley.edu>
2019-10-26 16:55:07 -0700

    logs sites upon update

--- background.js
+++ background.js
@@ -1,10 +1,37 @@
-chrome.commands.onCommand.addListener(function(command) {
-  if (command == "toggle-pin") {
-    // Get the currently selected tab
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      // Toggle the pinned status
-      var current = tabs[0]
-      chrome.tabs.update(current.id, {'pinned': !current.pinned});
-    });
-  }
+// chrome.commands.onCommand.addListener(function(command) {
+//   if (command == "toggle-pin") {
+//     // Get the currently selected tab
+//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//       // Toggle the pinned status
+//       var current = tabs[0]
+//       chrome.tabs.update(current.id, {'pinned': !current.pinned});
+//     });
+//   }
+// });
+
+// chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+//   console.log(tabs[0]);
+// });
+
+// chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+//     var url = tabs[0].url;
+//     console.log(url);
+// });
+
+chrome.tabs.onUpdated.addListener( function( tabId,  changeInfo,  tab) {
+       chrome.extension.getBackgroundPage().console.log(tab.url);
 });
+
+// Do NOT forget that the method is ASYNCHRONOUS
+// chrome.tabs.query({
+//     active: true,               // Select active tabs
+//     currentWindow: true     // In the current window
+// }, function(tabs) {
+//     // Since there can only be one active tab in one active window, 
+//     //  the array has only one element
+//     var tab = tabs[0];
+//     // Example:
+//     var url = tab.url;
+//     chrome.extension.getBackgroundPage().console.log(url);
+//     // ... do something with url variable
+// });
--- content.js
+++ content.js
@@ -1,1 +1,6 @@
-alert("Hello from your Chrome extension!")
+// var firstHref = $("a[href^='http']").eq(0).attr("href");
+// alert("yo");
+chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    var url = tabs[0].url;
+    alert(url);
+});
