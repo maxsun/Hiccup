a2d75ce96956f1cf61f7c62c4ba0f58f900f1b50
Shaurya Sanghvi <shaurya_s@berkeley.edu>
2019-10-26 18:17:52 -0700

    logs active sites, ts, title

--- background.js
+++ background.js
@@ -18,9 +18,40 @@
 //     console.log(url);
 // });
 
-chrome.tabs.onUpdated.addListener( function( tabId,  changeInfo,  tab) {
-       chrome.extension.getBackgroundPage().console.log(tab.url);
+// chrome.tabs.onActivated.addListener( function( tabId, windowId) {
+//     chrome.tabs.query({
+//     active: true,               // Select active tabs
+//     currentWindow: true     // In the current window
+// }, function(tabs) {
+//     // Since there can only be one active tab in one active window, 
+//     //  the array has only one element
+//     var tab = tabs[0];
+//     // Example:
+//     var url = tab.url;
+//     chrome.extension.getBackgroundPage().console.log(url);
+//     // ... do something with url variable
+// });
+// });
+
+
+chrome.tabs.onActivated.addListener(function(tab) {
+  // Send a message to the active tab
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
+    chrome.extension.getBackgroundPage().console.log(activeTab.url);
+    chrome.extension.getBackgroundPage().console.log(activeTab.title);
+    console.log(Date.now());
+  });
 });
+
+
+// console.logCopy = console.log.bind(console);
+
+// console.log = function(data)
+// {
+//     var currentDate = '[' + new Date().toUTCString() + '] ';
+//     this.logCopy(currentDate, data);
+// };
 
 // Do NOT forget that the method is ASYNCHRONOUS
 // chrome.tabs.query({
--- content.js
+++ content.js
@@ -1,6 +1,75 @@
 // var firstHref = $("a[href^='http']").eq(0).attr("href");
 // alert("yo");
-chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    var url = tabs[0].url;
-    alert(url);
-});
+
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//   	if (request.message === "activeTab") {
+// 	chrome.tabs.query({active: true,currentWindow: true}, function(tabs) {
+//     var tab = tabs[0];
+//     var url = tab.url;
+//     console.log(url);
+//     // ... do something with url variable
+// });
+//   	}
+//   }
+// );
+
+
+
+//background.js scratch work
+// console.logCopy = console.log.bind(console);
+
+// console.log = function(data)
+// {
+//     var currentDate = '[' + new Date().toUTCString() + '] ';
+//     this.logCopy(currentDate, data);
+// };
+
+// Do NOT forget that the method is ASYNCHRONOUS
+// chrome.tabs.query({
+//     active: true,               // Select active tabs
+//     currentWindow: true     // In the current window
+// }, function(tabs) {
+//     // Since there can only be one active tab in one active window, 
+//     //  the array has only one element
+//     var tab = tabs[0];
+//     // Example:
+//     var url = tab.url;
+//     chrome.extension.getBackgroundPage().console.log(url);
+//     // ... do something with url variable
+// });
+
+// chrome.commands.onCommand.addListener(function(command) {
+//   if (command == "toggle-pin") {
+//     // Get the currently selected tab
+//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//       // Toggle the pinned status
+//       var current = tabs[0]
+//       chrome.tabs.update(current.id, {'pinned': !current.pinned});
+//     });
+//   }
+// });
+
+// chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+//   console.log(tabs[0]);
+// });
+
+// chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+//     var url = tabs[0].url;
+//     console.log(url);
+// });
+
+// chrome.tabs.onActivated.addListener( function( tabId, windowId) {
+//     chrome.tabs.query({
+//     active: true,               // Select active tabs
+//     currentWindow: true     // In the current window
+// }, function(tabs) {
+//     // Since there can only be one active tab in one active window, 
+//     //  the array has only one element
+//     var tab = tabs[0];
+//     // Example:
+//     var url = tab.url;
+//     chrome.extension.getBackgroundPage().console.log(url);
+//     // ... do something with url variable
+// });
+// });
